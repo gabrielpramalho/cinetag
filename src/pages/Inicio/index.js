9d813f89c7911ace1ad933078eeea4b6113d3db0
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-10-29 15:17:42 -0300

    add Titulo

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,12 +2,16 @@ import Header from 'components/Header'
 import styles from './Inicio.module.css'
 import Footer from 'components/Footer'
 import Banner from 'components/Banner'
+import Titulo from 'components/Titulo'
 
 const Inicio = () =>{
     return(
         <>
             <Header />
             <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
             <Footer />
         </>
     )
