d36e1706a2a083bc0e08cb34af395bb8bb5e34ee
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-10-29 15:10:49 -0300

    add Banner

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,11 +1,13 @@
 import Header from 'components/Header'
 import styles from './Inicio.module.css'
 import Footer from 'components/Footer'
+import Banner from 'components/Banner'
 
 const Inicio = () =>{
     return(
         <>
             <Header />
+            <Banner imagem="home" />
             <Footer />
         </>
     )
